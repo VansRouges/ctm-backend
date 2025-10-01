388332e0f0c0fb70ee3cb897b84fce288f189238
Vance-sama <67249759+VansRouges@users.noreply.github.com>
2025-10-01 08:13:58 +0100

    Refactor Redis connection configuration

--- config/redis.js
+++ config/redis.js
@@ -10,10 +10,10 @@ class RedisClient {
     try {
       this.client = createClient({
         username: process.env.REDIS_USERNAME || 'default',
-        password: process.env.REDIS_PASSWORD || '8HvTbpfZsUhsy0imfdqjSHionn4Dhey3',
+        password: process.env.REDIS_PASSWORD,
         socket: {
-          host: process.env.REDIS_HOST || 'redis-17550.c10.us-east-1-4.ec2.redns.redis-cloud.com',
-          port: parseInt(process.env.REDIS_PORT) || 17550
+          host: process.env.REDIS_HOST,
+          port: parseInt(process.env.REDIS_PORT)
         }
       });
 
